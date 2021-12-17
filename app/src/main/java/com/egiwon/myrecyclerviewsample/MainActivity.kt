bc4e012e1ef1d21903fbac8e1644b9009b29d2bf
middlefgr <2giwon@gmail.com>
2021-12-17 22:47:50 +0900

    [Edit] - MainActivity -> BaseActivity derived

--- app/src/main/java/com/egiwon/myrecyclerviewsample/MainActivity.kt
+++ app/src/main/java/com/egiwon/myrecyclerviewsample/MainActivity.kt
@@ -1,19 +1,17 @@
 package com.egiwon.myrecyclerviewsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
+import com.egiwon.myrecyclerviewsample.base.BaseActivity
 import com.egiwon.myrecyclerviewsample.databinding.ActivityMainBinding
 import com.egiwon.myrecyclerviewsample.ext.startActivity
 import com.egiwon.myrecyclerviewsample.ui.autohorizontalscroll.AutoHorizontalScrollActivity
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : BaseActivity<ActivityMainBinding>(
+    R.layout.activity_main
+) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         onClick()
     }
 
