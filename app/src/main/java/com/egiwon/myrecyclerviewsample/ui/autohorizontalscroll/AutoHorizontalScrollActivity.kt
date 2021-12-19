d493f1d45a87c4b8cdc910f174b30ce8330758be
middlefgr <2giwon@gmail.com>
2021-12-19 23:00:26 +0900

    [Add] - ui -> viewModel (fetchRandomImages)

--- app/src/main/java/com/egiwon/myrecyclerviewsample/ui/autohorizontalscroll/AutoHorizontalScrollActivity.kt
+++ app/src/main/java/com/egiwon/myrecyclerviewsample/ui/autohorizontalscroll/AutoHorizontalScrollActivity.kt
@@ -1,9 +1,33 @@
 package com.egiwon.myrecyclerviewsample.ui.autohorizontalscroll
 
+import android.os.Bundle
+import androidx.activity.viewModels
 import com.egiwon.myrecyclerviewsample.R
 import com.egiwon.myrecyclerviewsample.base.BaseActivity
 import com.egiwon.myrecyclerviewsample.databinding.ActivityAutoScrollBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AutoHorizontalScrollActivity : BaseActivity<ActivityAutoScrollBinding>(
     R.layout.activity_auto_scroll
-)
+) {
+
+    private val viewModel: AutoScrollViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.rvImages.adapter = AutoHorizontalScrollAdapter(
+            R.layout.item_auto_scroll
+        )
+
+        binding.rvImages.setHasFixedSize(true)
+
+        viewModel.loadRandomImages(10)
+        setObserve()
+    }
+
+    private fun setObserve() {
+
+    }
+}
