ab980d061094c5383173dd687d00164d38911690
middlefgr <2giwon@gmail.com>
2021-12-20 22:20:58 +0900

    [Add] - network module -> rxjava adapter

--- app/src/main/java/com/egiwon/myrecyclerviewsample/di/NetworkModule.kt
+++ app/src/main/java/com/egiwon/myrecyclerviewsample/di/NetworkModule.kt
@@ -8,6 +8,7 @@ import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
@@ -25,6 +26,10 @@ class NetworkModule {
 
     @Provides
     @Singleton
+    fun provideRxCallAdapterFactory() = RxJava2CallAdapterFactory.create()
+
+    @Provides
+    @Singleton
     fun provideHttpClient(
         loggingInterceptor: HttpLoggingInterceptor,
         interceptor: Interceptor
@@ -37,10 +42,12 @@ class NetworkModule {
     @Singleton
     fun provideRetrofit(
         converterFactory: GsonConverterFactory,
+        rxJava2CallAdapterFactory: RxJava2CallAdapterFactory,
         client: OkHttpClient
     ): Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .client(client)
+        .addCallAdapterFactory(rxJava2CallAdapterFactory)
         .addConverterFactory(converterFactory)
         .build()
 
